edd4dd226f4c750e4e462b32fb06923e6f5befb7
Kevin Galligan <kgalligan@gmail.com>
2024-04-30 14:11:55 -0400

    Prep open source release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
